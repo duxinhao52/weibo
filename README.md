3e8a58bc4e98a731d2fc73073f350a8cc4b8922b
duxinhao52 <1923352347@qq.com>
2020-06-27 21:06:43 +0800

    简直要哭了
    不哭不哭不要哭

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# weibo
+# WeiboList
+
+2020/2/10 更新，见 Issue：https://github.com/Python3WebSpider/WeiboList/issues/9
---
+++ weibo.py
@@ -0,0 +1,47 @@
+import requests
+from pyquery import PyQuery as pq
+import json
+max_page=25
+base_url='https://m.weibo.cn/api/container/getIndex?'
+headers={
+    'Referer': 'https://m.weibo.cn/u/2830678474',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36',
+    'X-Requested-With': 'XMLHttpRequest'        #分辨出是Ajax请求的关键
+}
+def get_one_page(since_id):
+    params={
+        'type': 'uid',
+        'value': '2830678474',
+        'containerid': '1076032830678474',
+    }
+    if since_id!='':
+        params['since_id']=since_id
+    try:
+        response=requests.get(base_url,params=params,headers=headers)
+        if response.status_code==200:
+            return response.json()              #请求返回的是JSON格式的str类型，所以要解析
+        return None
+    except requests.RequestException:
+        return None
+def parse_one_page(text):
+    if text:
+        items=text.get('data').get('cards')
+        for item in items:
+            item=item.get('mblog')
+            weibo={}
+            weibo['id'] = item.get('id')
+            weibo['text'] = pq(item.get('text')).text()    #pq的text()方法可以删除文本内的html代码
+            weibo['attitudes'] = item.get('attitudes_count')
+            weibo['comments'] = item.get('comments_count')
+            weibo['reposts'] = item.get('reposts_count')
+            yield weibo
+def write_to_file(item):
+    with open('ret.txt','a',encoding='utf-8') as f:
+        f.write(json.dumps(item,ensure_ascii=False)+'\n')   #字典序列化，可写入文件,ensure_ascii可写入中文
+if __name__=='__main__':
+    since_id=''
+    for page in range(1,max_page+1):     #max_page为请求次数
+        text=get_one_page(since_id)
+        since_id=text.get('data').get('cardlistInfo').get('since_id')   #拿到下次的since_id
+        for item in parse_one_page(text):
+            write_to_file(item)
